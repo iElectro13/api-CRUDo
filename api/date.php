7a7be040d57c8d05a879ec9f72be5f0fb741a248
iElectro <ejose.acc@gmail.com>
2022-03-27 00:53:38 -0500

    Crud functionality

--- api/date.php
+++ api/date.php
@@ -2,21 +2,32 @@
 
     require '../classes/date-class.php';
     $dates = new Date();
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+    header('Content-Type: application/json; charset=utf-8');
 
 switch($_SERVER['REQUEST_METHOD']){
 
     case 'POST':
         $_POST = json_decode(file_get_contents('php://input'), true);
-        echo 'Guardar ' . $_POST['name'];
+        $response = $dates->newDate($_POST['name'], $_POST['topic'], $_POST['date']);
         break;
 
     case 'GET':
-        $response = $dates->getAllDates();
-        echo $response;
+        $id = $_GET['id'];
+        if($id){
+            $response = $dates->getById($id);
+            echo $response;
+        }else{
+            $response = $dates->getAllDates();
+            echo $response;
+        }
         break;
 
     case 'PUT':
-        echo 'Usuario editado correctamente';
+        $_PUT = json_decode(file_get_contents('php://input'), true);
+        $response = $dates->updateDate($_GET['id'], $_PUT['name'], $_PUT['topic'], $_PUT['date']);
         break;
 
     case 'DELETE':
--- classes/date-class.php
+++ classes/date-class.php
@@ -1,6 +1,10 @@
 <?php
     
     require '../config/db.php';
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+    header('Content-Type: application/json; charset=utf-8');
 
     class Date{
 
@@ -17,6 +21,8 @@
             }  
         }
 
+
+        //Get all from "date" table
         public function getAllDates(){
             $sql = "SELECT * from date";
             try{
@@ -24,15 +30,72 @@
                 if($resp->rowCount() > 0 ){
                     $dates = $resp->fetchAll(PDO::FETCH_OBJ);
                     echo json_encode($dates);
-                } else {
-                    echo json_encode("No hay ninguna cita");
                 }
 
             }catch(PDOException $e){
-                echo 'Error de consulta' . $e->getMessage();
+                echo 'Error de consulta: ' . $e->getMessage();
             }
         }
 
+        //Get date by id
+        public function getById($id){
+            $sql = "SELECT * from date WHERE date_id = $id";
+            try{
+                $resp = $this->db->query($sql);
+                if($resp->rowCount() > 0 ){
+                    $dates = $resp->fetchAll(PDO::FETCH_OBJ);
+                    echo json_encode($dates);
+                }
+
+            }catch(PDOException $e){
+                echo 'Error de consulta: ' . $e->getMessage();
+            }
+        }
+
+        //Create new date
+        public function newDate($name, $topic, $date){
+            $sql = "INSERT INTO date (name, topic, date) VALUES (:name, :topic, :date)";
+            try{
+                $resp = $this->db->prepare($sql);
+                $resp->bindParam(':name', $name);
+                $resp->bindParam(':topic', $topic);
+                $resp->bindParam(':date', $date);
+                $resp->execute();
+                if($resp->rowCount() > 0 ){
+                    $dates = $resp->fetchAll(PDO::FETCH_OBJ);
+                    echo json_encode($dates);
+                } else {
+                    echo json_encode("Nada creado");
+                }
+
+            }catch(PDOException $e){
+                echo 'Error de consulta: ' . $e->getMessage();
+            }
+        }
+
+        //Update a date
+        public function updateDate($id, $name, $topic, $date){
+            $sql = "UPDATE date SET name=:name, topic=:topic, date=:date WHERE date_id = :id";
+            try{
+                $resp = $this->db->prepare($sql);
+                $resp->bindParam(':id', $id);
+                $resp->bindParam(':name', $name);
+                $resp->bindParam(':topic', $topic);
+                $resp->bindParam(':date', $date);
+                $resp->execute();
+                if($resp->rowCount() > 0 ){
+                    $dates = $resp->fetchAll(PDO::FETCH_OBJ);
+                    echo json_encode($dates);
+                } else {
+                    echo json_encode("Nada actualizado");
+                }
+
+            }catch(PDOException $e){
+                echo 'Error de consulta: ' . $e->getMessage();
+            }
+        }
+
+        //Delete a single date from "date" table
         public function deleteSingle($id){
             $sql = "DELETE FROM date WHERE date_id = $id";
             try{
@@ -45,7 +108,7 @@
                 }
 
             }catch(PDOException $e){
-                echo 'Error de consulta' . $e->getMessage();
+                echo 'Error de consulta: ' . $e->getMessage();
             }
         }
     }
